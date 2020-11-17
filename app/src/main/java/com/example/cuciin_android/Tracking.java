7c91d41f8f78983b0785f6f8e197e5621d156ed9
tudeyanu <yanuadi5100@gmail.com>
2020-11-18 01:04:51 +0800

    Tracking View

---
+++ app/src/main/java/com/example/cuciin_android/Tracking.java
@@ -0,0 +1,14 @@
+package com.example.cuciin_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tracking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracking);
+    }
+}
