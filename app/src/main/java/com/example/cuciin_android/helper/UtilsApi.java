28ac30b763533f09093731d7cc6c1a444f848763
Sekigahara <androaprila12@gmail.com>
2021-01-16 11:40:34 +0700

    test

--- app/src/main/java/com/example/cuciin_android/helper/UtilsApi.java
+++ app/src/main/java/com/example/cuciin_android/helper/UtilsApi.java
@@ -1,7 +1,7 @@
 package com.example.cuciin_android.helper;
 
 public class UtilsApi {
-    public static final String BASE_URL_API="http://10.0.2.2:8000/api/auth/";
+    public static final String BASE_URL_API="http://api.cuci-in.me/api/auth/";
     public static final String BASE_MAPS_URL_API="https://maps.googleapis.com/maps/api/place/";
 
     public static ApiService getLocalAPIService(){
